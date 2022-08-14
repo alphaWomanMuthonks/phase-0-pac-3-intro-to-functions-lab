05405fae52a1e6dadf130110ac8552c6b467a27a
Joy Muthoni Malanga <jmalanga@icealion.com>
2022-08-14 18:35:03 +0300

    Working with functions

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string){
+    if (string.toLowerCase()===string){
+        console.log("I can\'t hear you!");
+    }
+    else if(string.toUpperCase()===string){
+        console.log("YES INDEED!");
+    }
+    else (string==="Let\'s have dinner together");{
+        console.log("I would love to!");
+
+    }
+
+}
